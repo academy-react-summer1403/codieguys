996466e01466e03a22d2e5d38c6e4e92a94859d7
AYaldaShabani <yalda.shabani27@gmail.com>
2024-10-04 20:44:02 +0330

    feat :  add side pic component for log/ reg

---
+++ src/components/common/Pic/index.jsx
@@ -0,0 +1,10 @@
+const Pic = ({pic}) => {
+    return(
+        <div className="bg-magic-mint w-[769px] h-full rounded-l-[20px] ">
+              <div className="w-[621.42px] h-[578.16px] ml-[74px] mt-[139px] inline-block ">
+                <img src={pic} alt="" />
+              </div>
+            </div>
+    )
+    }
+    export default Pic
