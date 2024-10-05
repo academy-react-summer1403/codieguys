1b6022e0eb57742d35f78fd242593914473cd64d
AYaldaShabani <yalda.shabani27@gmail.com>
2024-10-05 13:23:38 +0330

    feat :  add register step 1 design

---
+++ src/components/AuthLayout/RegisterHolder/RegisterHolderStep1/RegisterFormHolder.jsx
@@ -0,0 +1,29 @@
+import { Formik , Form } from "formik";
+import FormField from "../../../common/FormField";
+import LongButtonGreen from "../../../common/Buttons/LongButtonGreen";
+import LongButtonWhite from "../../../common/Buttons/LongButtonWhite";
+import mobileIcon from "../../../../assets/login/Path 1905.png"
+const RegisterForm = () => {
+  return (
+    <>
+      <div className="w-[773px] h-[750px] inline-block  ">
+        <h5 className="font-normal leading-[48.41px] text-[40px] text-right mr-[65px] ">
+ثبت نام         </h5>
+        <Formik>
+          <Form className="ml-[72px] mt-[79px]">
+            <div className="mb-[101px]">
+              <FormField placeholder="شمماره همراه" src={mobileIcon} />
+            </div>
+
+            <LongButtonGreen name="تایید" />
+            <LongButtonWhite name="بازگشت به صفحه اصلی" />
+          </Form>
+        </Formik>
+        <span className="underline font-normal text-xl text-right text-[#22445D] ml-[500px]">
+        من_از_قبل_عضو_هستم
+      </span>
+      </div>
+    </>
+  );
+};
+export default RegisterForm;
--- src/components/AuthLayout/RegisterHolder/RegisterHolderStep1/index.jsx
+++ src/components/AuthLayout/RegisterHolder/RegisterHolderStep1/index.jsx
@@ -1,16 +1,9 @@
+import Layout from "../../Layout";
+import register from "../../../../assets/register/register.png"
+import RegisterForm from "./registerFormHolder";
 const RegisterHolderStep1 = () => {
   return (
-    <>
-      <div className=" flex mt-[112px] mx-auto w-[1539px] h-[857px] bg-custom-gray drop-shadow-[10px_10px_5px_rgba(0,0,0,0.16)] rounded-[20px]">
-        <Pic pic={verify} />
-        <div>
-          <div className="w-8 h-8 mt-9 ml-9">
-            <img src={homeLogo} alt="" />
-          </div>
-          <LoginForm />
-        </div>
-      </div>
-    </>
+    <Layout picSrc={register} pageForm={<RegisterForm />} />
   );
 };
 
