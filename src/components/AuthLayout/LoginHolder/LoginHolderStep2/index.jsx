2bde2301271cbc8e1d14309c33c2093faf35e082
AYaldaShabani <yalda.shabani27@gmail.com>
2024-10-05 13:00:39 +0330

    fix : component prop bug fix

--- src/components/AuthLayout/LoginHolder/LoginHolderStep2/index.jsx
+++ src/components/AuthLayout/LoginHolder/LoginHolderStep2/index.jsx
@@ -5,6 +5,6 @@ import LoginFormStep2 from "./LoginFormStep2";
 import Layout from "../../Layout";
 
 const LoginHolderStep2 = () => {
-  return <Layout picSrc={sign} pageForm={LoginFormStep2} />;
+  return <Layout picSrc={sign} pageForm={<LoginFormStep2 />} />;
 };
 export default LoginHolderStep2;
