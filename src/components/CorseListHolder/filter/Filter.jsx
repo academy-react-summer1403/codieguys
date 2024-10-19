1b035776f0ae5258dcdc6f7006c797d732d969d9
parinaz544 <parinazfirouzi544@gmail.com>
2024-10-19 14:36:15 +0330

    filter updated

--- src/components/CorseListHolder/filter/Filter.jsx
+++ src/components/CorseListHolder/filter/Filter.jsx
@@ -4,27 +4,35 @@ import down from "../../../assets/btn/Path 1881.png";
 
 const Filter = () => {
   return (
-    <div className="relative w-[399px] h-[926px] rounded-[37px] shadow-2xl border-2 border-slate-400 bg-custom-gray">
-      <div className="w-[109.87px] h-[31.69px] absolute   mt-[40px]">
-        <img
-          src={filter}
-          className="w-[28.81px] h-[26.19px] absolute top-[8px] left-[220px] "
-        />
-        <h1 className="text-[25px] text-slate-600 absolute left-[140px]">
-          فیلترها
-        </h1>
+    <div className="w-[350px] h-[935px] shadow-2xl rounded-[37px] bg-custom-gray">
+      <div className="flex justify-center gap-2 my-12">
+        <img src={filter} className="w-[28.81px] h-[26.19px]" />
+        <h1 className="text-2xl">فیلترها</h1>
       </div>
-
-      <div className="w-[337px] border-b-4 border-lightGreen absolute top-[150px] left-[30px]">
-        <h2 className="text-[25px] text-slate-600 absolute -top-[50px] left-[220px]">
-          تکنولوژی
-        </h2>
-        <img src={down} className="absolute -top-[30px] left-[10px]" />
+      <div className="mx-8">
+          <div className="">
+            <h2>تکنولوژی</h2>
+          </div>
       </div>
-
-      
-
     </div>
+    // <div className="relative w-[399px] h-[926px] rounded-[37px] shadow-2xl border-2 border-slate-400 bg-custom-gray">
+    //   <div className="w-[109.87px] h-[31.69px] absolute   mt-[40px]">
+    //     <img
+    //       src={filter}
+    //       className="w-[28.81px] h-[26.19px] absolute top-[8px] left-[220px] "
+    //     />
+    //     <h1 className="text-[25px] text-slate-600 absolute left-[140px]">
+    //       فیلترها
+    //     </h1>
+    //   </div>
+
+    //   <div className="w-[337px] border-b-4 border-lightGreen absolute top-[150px] left-[30px]">
+    //     <h2 className="text-[25px] text-slate-600 absolute -top-[50px] left-[220px]">
+    //       تکنولوژی
+    //     </h2>
+    //     <img src={down} className="absolute -top-[30px] left-[10px]" />
+    //   </div>
+    // </div>
   );
 };
 
