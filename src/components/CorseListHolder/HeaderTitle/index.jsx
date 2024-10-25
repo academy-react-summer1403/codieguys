125d7d61ad48e59f4856c5220398ac0c591df61b
parinaz544 <parinazfirouzi544@gmail.com>
2024-10-26 01:19:29 +0330

    feat :

--- src/components/CorseListHolder/HeaderTitle/index.jsx
+++ src/components/CorseListHolder/HeaderTitle/index.jsx
@@ -1,7 +1,7 @@
-const HeaderTitle = ()=>{
+const HeaderTitle = ({title})=>{
     return(
         <div className="flex justify-center py-10">
-            <h2 className="text-6xl text-slate-700">لیست دوره ها</h2>
+            <h2 className="text-6xl text-slate-700">{"لیست دوره ها"}</h2>
         </div>
     );
 }
