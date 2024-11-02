ee5b80a5322d78bad34141f10535cb394c89c8bf
parinaz544 <parinazfirouzi544@gmail.com>
2024-11-02 14:42:13 +0330

    feat : updated

--- src/config/router/mainRoutes.jsx
+++ src/config/router/mainRoutes.jsx
@@ -1,11 +1,13 @@
 import CorseDetail from "../../screens/CorseDetail";
 import Landing from "../../screens/Landing";
+import CorseListHolder from "../../components/CorseListHolder";
+import NewsListHolder from "../../components/NewsListHolder";
 
 const mainRoutes = [
     { path: "/CorseDetail", element: <CorseDetail /> },
     {path:"/Landing" , element: <Landing /> },
-    // {path:"/CourseList" , element: <CorseListHolder /> }
-
+    {path:"/CourseList" , element: <CorseListHolder /> },
+    {path:"/NewsList" , element: <NewsListHolder /> }
   
   
   ];
